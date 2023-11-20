4e72e59a0bfa82897ac4c0acaef7a15845a68b00
bikashroy120 <bikashroydt@gmail.com>
2023-11-20 17:52:50 +0600

    add

--- src/Components/SuperAdmin/superAdmin/SuperAdminUserTable.jsx
+++ src/Components/SuperAdmin/superAdmin/SuperAdminUserTable.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import AdminFiled from '../../../Shared/AdminFiled/AdminFiled';
 import CustomTable from '../../../Shared/Table/CustomTable';
+import SuperAdminUserTableAction from './SuperAdminUserTableAction';
+import { Icon } from '@iconify/react';
 
 const SuperAdminUserTable = ({tableData}) => {
     const columns = [
@@ -23,13 +25,13 @@ const SuperAdminUserTable = ({tableData}) => {
             title: 'BUSINESS ADDRESS',
             render: (row) => <span className='text-[16px] font-[400] text-text-body'>{row.businessAdress}</span>,
         },
-        // {
-        //     title: '',
-        //     key: 'action',
-        //     render: (row) => (
-        //         <SuperAdminUserDataAction data={row} />
-        //     )
-        // }
+        {
+            title: <Icon icon="basil:other-2-outline" className=" text-[35px]"/>,
+            key: 'action',
+            render: (row) => (
+                <SuperAdminUserTableAction data={row} />
+            )
+        }
     ];
 
   return (
---
+++ src/Components/SuperAdmin/superAdmin/SuperAdminUserTableAction.jsx
@@ -0,0 +1,29 @@
+import { Icon } from "@iconify/react";
+import { Button, Popover } from "antd";
+import React from "react";
+
+const SuperAdminUserTableAction = ({data}) => {
+
+
+    // =====Action button Edit Reset Delete=====
+    const content = (
+        <div className=" w-[170px]">
+            <button className=" text-[14px] w-full items-start rounded-[10px] font-[500] text-[#969BB3] hover:bg-[#F4EBFF] hover:text-[#9039FF] flex  py-3 px-5">Edit Details</button>
+            <button className=" text-[14px] flex  w-full items-start rounded-[10px] font-[500] text-[#969BB3] hover:bg-[#F4EBFF] hover:text-[#9039FF] py-3 px-5">Reset password</button>
+            <button className=" text-[14px] w-full flex  items-start rounded-[10px] font-[500] text-[#969BB3] hover:bg-[#FDEEEE] hover:text-[#FF5959] py-3 px-5">Delete Admin</button>
+        </div>
+      );
+
+
+  return (
+    <div>
+      <div>
+        <Popover content={content} placement="leftTop" trigger="click">
+          <button className=" hover:bg-primary/10 rounded-full w-[40px] h-[40px] flex items-center justify-center"><Icon icon="basil:other-2-outline" className=" text-[35px]"/></button>
+        </Popover>
+      </div>
+    </div>
+  );
+};
+
+export default SuperAdminUserTableAction;
