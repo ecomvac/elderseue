476ce3cea95d6f90ea71cd6703c77b6ac057c878
bikashroy120 <bikashroydt@gmail.com>
2023-12-02 14:08:33 +0600

    logout modal add

--- src/Components/SupportAgent/SupportAgentSidebar/SupportAgentSidebar.jsx
+++ src/Components/SupportAgent/SupportAgentSidebar/SupportAgentSidebar.jsx
@@ -1,11 +1,17 @@
 import { Icon } from "@iconify/react";
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import SupportAgentSidebarProgress from "./SupportAgentSidebarProgress";
+import LogOutModal from "../../../Shared/LogOut/LogOutModal";
 
 const SupportAgentSidebar = () => {
   const navigate = useNavigate();
   const path = useLocation();
+  const [show, setShow] = useState(false);
+
+  const handelLogout = () => {
+    navigate("/");
+  };
 
   const activeStyle = {
     color: "#fff",
@@ -14,73 +20,80 @@ const SupportAgentSidebar = () => {
       "linear-gradient(90deg, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.00) 104.88%)",
   };
   return (
-    <div className="flex flex-col justify-between bg-[#2C0064] h-[100vh] sticky top-0">
-      <section className="w-[280px] ">
-        <div className="h-[80px] flex mt-5 items-center justify-center">
-          {" "}
-          <img
-            src="/images/sidebarlogo.png"
-            alt="logo"
-            className="max-w-[143px] mx-auto"
-          />
-        </div>
-        {/* sidebar menu bar */}
-        <div className="py-4 grid gap-1">
-          <NavLink
-            to={"/support-agent/dashboard"}
-            id="sidebar_hover"
-            className="block p-4 duration-150 text-white/60"
-            style={
-              path.pathname === "/support-agent/dashboard"
-                ? activeStyle
-                : undefined
-            }
-          >
-            <div className="flex item-center gap-2 font-bold">
-              <span>
-                <Icon className=" w-5 h-5" icon="basil:info-triangle-outline" />
-              </span>
-              <p className="text-[16px] font-[500]">Alerts</p>
-            </div>
-          </NavLink>
-          <NavLink
-            to={"/support-agent/dashboard/elderly"}
-            id="sidebar_hover"
-            className="block p-4 duration-150 text-white/60"
-            style={
-              path.pathname.slice(0, 32) === "/support-agent/dashboard/elderly"
-                ? activeStyle
-                : undefined
-            }
-          >
-            <div className="flex item-center gap-2 font-bold">
-              <span>
-                <Icon className=" w-5 h-5" icon="basil:user-outline" />
-              </span>
-              <p className="text-[16px] font-[500]">Elderly</p>
-            </div>
-          </NavLink>
-        </div>
-      </section>
+    <>
+      <div className="flex flex-col justify-between bg-[#2C0064] h-[100vh] sticky top-0">
+        <section className="w-[280px] ">
+          <div className="h-[80px] flex mt-5 items-center justify-center">
+            {" "}
+            <img
+              src="/images/sidebarlogo.png"
+              alt="logo"
+              className="max-w-[143px] mx-auto"
+            />
+          </div>
+          {/* sidebar menu bar */}
+          <div className="py-4 grid gap-1">
+            <NavLink
+              to={"/support-agent/dashboard"}
+              id="sidebar_hover"
+              className="block p-4 duration-150 text-white/60"
+              style={
+                path.pathname === "/support-agent/dashboard"
+                  ? activeStyle
+                  : undefined
+              }
+            >
+              <div className="flex item-center gap-2 font-bold">
+                <span>
+                  <Icon
+                    className=" w-5 h-5"
+                    icon="basil:info-triangle-outline"
+                  />
+                </span>
+                <p className="text-[16px] font-[500]">Alerts</p>
+              </div>
+            </NavLink>
+            <NavLink
+              to={"/support-agent/dashboard/elderly"}
+              id="sidebar_hover"
+              className="block p-4 duration-150 text-white/60"
+              style={
+                path.pathname.slice(0, 32) ===
+                "/support-agent/dashboard/elderly"
+                  ? activeStyle
+                  : undefined
+              }
+            >
+              <div className="flex item-center gap-2 font-bold">
+                <span>
+                  <Icon className=" w-5 h-5" icon="basil:user-outline" />
+                </span>
+                <p className="text-[16px] font-[500]">Elderly</p>
+              </div>
+            </NavLink>
+          </div>
+        </section>
 
-      <div>
         <div>
-            <SupportAgentSidebarProgress/>
-        </div>
+          <div>
+            <SupportAgentSidebarProgress />
+          </div>
 
-        <div className="flex justify-between items-center group p-4">
-          <button
-            onClick={() => navigate("/")}
-            className="flex gap-2 w-full py-1 cursor-pointer justify-between rounded-[8px] px-3 bg_color_gradient  items-center"
-          >
-            <p className="text-[16px] font-[500] text-white">Log out</p>
-            <span className=" text-white w-[40px] flex items-center justify-center rounded-full h-[40px]">
-              <Icon icon="humbleicons:logout" className=" text-[25px]" />
-            </span>
-          </button>
+          <div className="flex justify-between items-center group p-4">
+            <button
+              onClick={() => setShow(true)}
+              className="flex gap-2 w-full py-1 cursor-pointer justify-between rounded-[8px] px-3 bg_color_gradient  items-center"
+            >
+              <p className="text-[16px] font-[500] text-white">Log out</p>
+              <span className=" text-white w-[40px] flex items-center justify-center rounded-full h-[40px]">
+                <Icon icon="humbleicons:logout" className=" text-[25px]" />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <LogOutModal modalOPen={show} setModalOpen={setShow} onDelete={handelLogout} />
+    </>
   );
 };
 
---
+++ src/Shared/LogOut/LogOutModal.jsx
@@ -0,0 +1,75 @@
+import { Icon } from "@iconify/react";
+import { Modal } from "antd";
+import React from "react";
+
+const LogOutModal = ({
+  modalOPen,
+  setModalOpen,
+  className,
+  onDelete,
+}) => {
+  return (
+    <div className="">
+      <Modal
+        centered
+        cancelText
+        cancelButtonProps
+        footer={null}
+        open={modalOPen}
+        closeIcon={null}
+        styles={{borderRadius:30}}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
+        width={520}
+        className={` bg-primary pt-3 rounded-[30px]`}
+      >
+        <div className=" bg-primary pt-10 rounded-[30px] h-14  -z-20 relative left-0 top-[-10px]"></div>
+        <div className=" rounded-[30px] mt-[-40px]">
+          <div className=" flex items-center justify-end mr-5">
+            <button
+              onClick={() => setModalOpen(false)}
+              className=" w-[40px] text-3xl h-[40px] rounded-lg flex items-center justify-center hover:bg-[#FDEEEE] hover:text-[#FF5959] text-[#969BB3]"
+            >
+              <Icon icon="material-symbols:close" />
+            </button>
+          </div>
+
+          <div style={{borderRadius:"16px"}} className=" w-full flex items-center justify-center py-3 pb-[40px]">
+            <div className=" w-[110px] h-[110px] rounded-full border-[5px] border-dashed border-[#EDE5FF] bg-[#FAF6FF] flex items-center justify-center">
+              <Icon
+                icon="line-md:log-out"
+                className="text-[45px] text-[#9039FF]"
+              />
+            </div>
+          </div>
+
+          <div>
+            <h2 className=" text-[25px] text-center font-[500] text-text-primary">
+                 Log Out
+            </h2>
+            <p className="text-[16px] font-[400] text-[#707EAE] text-center">
+                Are You Sure You Want To Log Out?
+            </p>
+          </div>
+        </div>
+
+        <div className=" flex items-center justify-center gap-5 pt-[40px] pb-9">
+          <button
+            onClick={()=>{onDelete();setModalOpen(false)}}
+            className="font-bold  h-[40px] px-6 rounded-[10px] bg-primary  border-primary text-white "
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => setModalOpen(false)}
+            className="font-bold  h-[40px] px-6 rounded-[10px] bg-transparent text-secondary border border-secondary"
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default LogOutModal;
