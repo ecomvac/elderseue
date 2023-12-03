38b545c85553017ebaff9e89c202a3b688471626
Tahmina <tahminakhatun5447@gmail.com>
2023-12-03 18:01:27 +0600

    fix chart design

--- src/Components/Admin/Elderly/SingleElderly/Customers/MembersOverview.jsx
+++ src/Components/Admin/Elderly/SingleElderly/Customers/MembersOverview.jsx
@@ -5,6 +5,80 @@ import { Icon } from '@iconify/react';
 import MemberChat from './MemberChat';
 
 const MembersOverview = () => {
+    // function renderStatistic(containerWidth, text, fontSize, fontWeight) {
+    //     const { width: textWidth, height: textHeight } = measureTextWidth(text, fontSize);
+    //     const R = containerWidth / 2; 
+    //     let scale = 1;
+
+    //     if (containerWidth < textWidth) {
+    //         scale = Math.min(Math.sqrt(Math.abs(Math.pow(R, 2) / (Math.pow(textWidth / 2, 2) + Math.pow(textHeight, 2)))), 1);
+    //     }
+
+    //     const textStyleStr = `width:${containerWidth}px;`;
+    //     return `<div style="${textStyleStr};font-size:${fontSize}px; font-weight:${fontWeight}; line-height:${scale < 1 ? 1 : 'inherit'};">${text}</div>`;
+    // }
+
+    const data = [
+        {
+            type: 'Male',
+            value: 6,
+        },
+        {
+            type: 'Female',
+            value: 4,
+        },
+
+    ]
+    // const config = {
+    //     appendPadding: 100,
+    //     data,
+    //     legend: false,
+    //     angleField: 'value',
+    //     colorField: 'type',
+    //     radius: 1,
+    //     startAngle: (-5 / 5) * Math.PI,
+    //     endAngle: (0 / 5) * Math.PI,
+    //     innerRadius: 0.60,
+    //     color: ['#ffffff', '#d3b0ff'],
+    //     label: {
+    //         type: 'inner',
+    //         offset: '-50%',
+    //         style: {
+    //             textAlign: 'center',
+    //             fontSize: 0
+    //         },
+    //         autoRotate: true,
+    //         content: '{value}',
+    //     },
+    //     statistic: {
+    //         title: {
+    //             offsetY: 65,
+    //             customHtml: (container, view, datum, data) => {
+    //                 const { width, height } = container.getBoundingClientRect();
+    //                 const d = Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2));
+    //                 const text = datum ? ` ${datum.value}` : ` ${data.reduce((r, d) => r + d.value, 0)}`;
+    //                 return renderStatistic(d, text, 28, 700);
+    //             },
+    //         },
+    //         content: {
+    //             offsetY: 70,
+    //             customHtml: (container, view, datum,) => {
+    //                 const { width } = container.getBoundingClientRect();
+    //                 const text = datum ? datum.type : 'Total Member';
+    //                 return renderStatistic(width, text, 12, 500);
+    //             },
+    //         },
+    //     },
+    //     interactions: [
+    //     ],
+    //     tooltip: {
+    //         customContent: (title, items) => {
+    //             return (
+    //                 <></>
+    //             );
+    //         },
+    //     },
+    // };
     function renderStatistic(containerWidth, text, fontSize, fontWeight) {
         const { width: textWidth, height: textHeight } = measureTextWidth(text, fontSize);
         const R = containerWidth / 2; // r^2 = (w / 2)^2 + (h - offsetY)^2
@@ -19,19 +93,22 @@ const MembersOverview = () => {
         return `<div style="${textStyleStr};font-size:${fontSize}px; font-weight:${fontWeight}; line-height:${scale < 1 ? 1 : 'inherit'};">${text}</div>`;
     }
 
-    const data = [
-        {
-            type: 'Male',
-            value: 6,
-        },
-        {
-            type: 'Female',
-            value: 4,
-        },
+    const calculatePercentage = (data, gender) => {
+        // Filter data for the specified gender
+        const filteredData = data.filter(item => item.type === gender);
 
-    ]
+        // Calculate the total value for the specified gender
+        const totalValue = filteredData.reduce((sum, item) => sum + item.value, 0);
+        const total = data.reduce((sum, item) => sum + item.value, 0);
+
+        // Calculate the percentage for the specified gender
+        const percentage = (totalValue / total) * 100;
+
+        return percentage;
+    };
+
     const config = {
-        appendPadding: 100,
+        appendPadding: 10,
         data,
         legend: false,
         angleField: 'value',
@@ -51,6 +128,13 @@ const MembersOverview = () => {
             autoRotate: true,
             content: '{value}',
         },
+        tooltip: {
+                    customContent: (title, items) => {
+                        return (
+                            <></>
+                        );
+                    },
+                },
         statistic: {
             title: {
                 offsetY: 65,
@@ -58,7 +142,7 @@ const MembersOverview = () => {
                     const { width, height } = container.getBoundingClientRect();
                     const d = Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2));
                     const text = datum ? ` ${datum.value}` : ` ${data.reduce((r, d) => r + d.value, 0)}`;
-                    return renderStatistic(d, text, 28, 700);
+                    return renderStatistic(d, text, 45, 600);
                 },
             },
             content: {
@@ -66,7 +150,7 @@ const MembersOverview = () => {
                 customHtml: (container, view, datum,) => {
                     const { width } = container.getBoundingClientRect();
                     const text = datum ? datum.type : 'Total Member';
-                    return renderStatistic(width, text, 12, 500);
+                    return renderStatistic(width, text, 14, 500);
                 },
             },
         },
@@ -75,41 +159,46 @@ const MembersOverview = () => {
     };
     return (
         <>
+            {/* ' */}
             <SectionWrapper>
-                <div id='member' className='bg-primary rounded-[20px] p-6'>
+                <div className='bg-primary rounded-[20px] p-6'>
                     <div>
                         <h2 className='text-white text-2xl font-bold'>Members Overview</h2>
                     </div>
                     <div className='mt-12'>
-                        <div className='flex items-center justify-between'>
-                            <div className='w-[79px] flex flex-col gap-5'>
-                                <span className='flex flex-col'>
-                                    <span className='flex items-center gap-1 text-base text-secondLightPrimary font-medium'>
-                                        <span><Icon icon="ion:male" /></span>
-                                        <span>Male</span>
+                        <div className='flex flex-col xl:flex-row md:flex-row  gap-5 items-center justify-between'>
+                            <div className=''>
+                                <div className='w-[79px] flex flex-col gap-5'>
+                                    <span className='flex flex-col'>
+                                        <span className='flex items-center gap-1 text-base text-secondLightPrimary font-medium'>
+                                            <span><Icon icon="ion:male" /></span>
+                                            <span>Male</span>
+                                        </span>
+                                        <span className='text-[34px] font-bold text-white'>06</span>
                                     </span>
-                                    <span className='text-[34px] font-bold text-white'>06</span>
-                                </span>
-                                <span className='flex flex-col'>
-                                    <span className='flex items-center gap-1 text-base text-secondLightPrimary font-medium'>
-                                        <span><Icon className='text-2xl' icon="material-symbols:female" /></span>
-                                        <span>Female</span>
+                                    <span className='flex flex-col'>
+                                        <span className='flex items-center gap-1 text-base text-secondLightPrimary font-medium'>
+                                            <span><Icon className='text-2xl' icon="material-symbols:female" /></span>
+                                            <span>Female</span>
+                                        </span>
+                                        <span className='text-[34px] font-bold text-white'>04</span>
                                     </span>
-                                    <span className='text-[34px] font-bold text-white'>04</span>
-                                </span>
+                                </div>
                             </div>
-                            <div className='mt-[-158px]'>
-                                <Pie className='w-[400px]' {...config} />
-                                <div className='flex justify-center mt-[-147px]'>
-                                    <div className='flex items-center w-[200px] justify-between text-white text-base font-bold'>
-                                        <span>60%</span>
-                                        <span>40%</span>
+
+                            <div className='flex justify-center'>
+                                <div id='member' className='w-[245px] lg2:w-[200px] xl:w-[245px] h-[158px] text-white '>
+                                    <Pie {...config} />
+                                    <div className='flex items-center justify-between mt-[-20px] px-3'>
+                                        <h2 className=' text-xl font-bold text-white'>{calculatePercentage(data, "Male").toFixed(0)}%</h2>
+                                        <h2 className=' text-xl font-bold text-white'>{calculatePercentage(data, "Female").toFixed(0)}%</h2>
                                     </div>
                                 </div>
                             </div>
+                           
                         </div>
                     </div>
-                    <div className='mt-[106px]'> 
+                    <div className='mt-[106px]'>
                         <div className='mb-2.5 px-[35px] flex items-center justify-between'>
                             <span className='flex flex-col'>
                                 <span className='text-[28px] font-bold text-white'>01</span>
@@ -127,14 +216,15 @@ const MembersOverview = () => {
                                 <span className='text-[28px] font-bold text-white'>02</span>
                                 <span className='font-medium text-[13px] text-secondLightPrimary'>Elderly</span>
                             </span>
-                            
+
                         </div>
                         <div></div>
                     </div>
                     <div>
-                        <MemberChat/>
+                        <MemberChat />
                     </div>
                 </div>
+              
 
             </SectionWrapper>
 
