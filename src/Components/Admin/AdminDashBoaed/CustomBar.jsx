a656a88aba770b67f6b13518e7fbb74e964221b9
Tahmina <tahminakhatun5447@gmail.com>
2023-12-03 18:07:26 +0600

    update

--- src/Components/Admin/AdminDashBoaed/CustomBar.jsx
+++ src/Components/Admin/AdminDashBoaed/CustomBar.jsx
@@ -7,7 +7,7 @@ const CustomBar = () => {
   return (
     <div className='w-[237px] h-[18px] flex items-center gap-[3px] rounded-full'>
 
-      <div style={{ width: problem }} className={`cursor-pointer h-full bg-[#5C76FF] relative group`}>
+      <div style={{ width: problem }} className={`cursor-pointer rounded-s-full h-full bg-[#5C76FF] relative group`}>
         <div className=' bg-text-primary rounded-[10px] p-2.5 text-white absolute top-[-10px] left-1 group-hover:flex items-center gap-2 duration-300 hidden '>
           <div className='h-2 w-2 rounded-full bg-[#5C76FF]'></div>
           <p className='text-white text-[13px]'>{problem}</p>
@@ -21,7 +21,7 @@ const CustomBar = () => {
         </div>
       </div>
 
-      <div style={{ width: critical }} className={`cursor-pointer  h-full bg-[#F83333] group relative`}>
+      <div style={{ width: critical }} className={`cursor-pointer rounded-e-full h-full bg-[#F83333] group relative`}>
         <div className=' bg-text-primary rounded-[10px] p-2.5 text-white absolute top-[-10px] left-1 group-hover:flex items-center gap-2 duration-300 hidden '>
           <div className='h-2 w-2 rounded-full bg-[#F83333]'></div>
           <p className='text-white text-[13px]'>{critical}</p>
