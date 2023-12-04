a9a5064b8ab62e72a01b3564fe454da1b4dbe887
Tahmina <tahminakhatun5447@gmail.com>
2023-12-04 14:56:23 +0600

    add inner device

--- src/Components/Admin/Elderly/SingleElderly/Devices/InnerWave/DailyOverView.jsx
+++ src/Components/Admin/Elderly/SingleElderly/Devices/InnerWave/DailyOverView.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { waveData } from '../../../../../../assets/singleElderlyData'
 const DailyOverView = () => {
     return (
         <div className='px-6 py-[18px]'>
             <h1 className='font-semibold text-text-primary text-2xl mb-9'>Daily Overview</h1>
             <div>
-
+                <div className='grid grid-cols-1 md:grid-col-2 gap-y-6'>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Components/Admin/Elderly/SingleElderly/Devices/InnerWave/InnerWave.jsx
+++ src/Components/Admin/Elderly/SingleElderly/Devices/InnerWave/InnerWave.jsx
@@ -2,19 +2,26 @@ import React, { useContext } from 'react';
 import SectionWrapper from '../../../../../../Shared/SectionWrapper';
 import { SidebarContext } from '../../../../../../Context/CustomContext';
 import { Icon } from '@iconify/react';
+import DailyOverView from './DailyOverView';
 
 const InnerWave = () => {
-    
+
     const { setDeviceInner } = useContext(SidebarContext)
     return (
         <div className='mt-[30px] mb-12'>
-        <SectionWrapper>
-            <div className='flex items-center px-[22px] pt-6 pb-[30px] gap-3 border-b'>
-                <button onClick={() => setDeviceInner('')} className='border flex items-center justify-center rounded-full p-2 text-xl text-text-primary/60'><Icon icon="fe:arrow-left" /></button><h2 className='text-text-primary font-bold text-2xl'>Wave – Vital Signs Monitor</h2>
-            </div>
-
-        </SectionWrapper>
-    </div>
+            <SectionWrapper>
+                <div className='flex items-center px-[22px] pt-6 pb-[30px] gap-3 border-b'>
+                    <button onClick={() => setDeviceInner('')} className='border flex items-center justify-center rounded-full p-2 text-xl text-text-primary/60'><Icon icon="fe:arrow-left" /></button><h2 className='text-[#2B3674] font-bold text-2xl'>Wave – Vital Signs Monitor</h2>
+                </div>
+                <div className='xl:flex'>
+                    <div className='xl:w-1/3 w-full'>
+                        <DailyOverView />
+                        {/* viobita */}
+                    </div>
+                    <div className='xl:w-2/3 w-full'></div>
+                </div>
+            </SectionWrapper>
+        </div>
     );
 };
 
